e8966481e83d90db0d6d5bc5f89a700622f16f0c
rutvikkapade <46281245+rutvikkapade@users.noreply.github.com>
2020-01-26 15:22:10 +0530

    pdcsvText.py
    CSV support added

--- PdfToText.py
+++ PdfToText.py
@@ -1,11 +1,13 @@
+import tabula as tesseract
 import PyPDF2
 
-pdf=open("sample.pdf","rb") #put any pdf file here 
+
+pdf=open("sample1.pdf","rb") #put any pdf file here 
 pdata=PyPDF2.PdfFileReader(pdf);
 nop=pdata.numPages #no. of pages stored
 pob=pdata.getPage(nop-1) #subrtracts one from nop index[0]
 text=pob.extractText() #extracts text to text object file
-
+tesseract.convert_into("sample1.pdf", "output.csv", output_format="csv", pages='all')
 file=open(r"test.text","a"); #writes file in append mode
 file.writelines(text)
 file.close()
