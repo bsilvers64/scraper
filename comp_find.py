34028184ebeb601b706b516702a4e35ac828eebb
bsilvers64 <48654366+bsilvers64@users.noreply.github.com>
2020-02-01 01:39:29 +0530

    Update comp_find.py

--- comp_find.py
+++ comp_find.py
@@ -1,25 +1,33 @@
-
 import csv
-import pandas as pd
-data=[]
-with open("a.csv",'r') as csvfile:
-        reader=csv.reader(csvfile)
+data = []
+
+
+def compfind(file="corporate_data.csv"):
+    with open(file, 'r') as csvfile:
+        reader = csv.reader(csvfile)
         for row in reader:
             data.append(row)
 
-name = input("enter a file name : ").lower()
+    name = input("enter a company name : ").lower()
 
-j=0
-for i in data:
-    i = [x.strip().lower() for x in i]
-    data[j] = i
-    j += 1
+    j = 0
+    for i in data:
+        i = [x.strip().lower() for x in i]
+        data[j] = i
+        j += 1
 
-col= [x[0].strip() for x in data]
+    col = [x[0].strip() for x in data if x]
+    if name in col:
+        for x in range(0, len(data)):
+            if name == data[x][0]:
+                print(data[x])
+    else:
+        print("name doesn't exist")
 
-if name in col:
-    for x in range(0, len(data)):
-     if name == data[x][0]:
-        print(data[x])
-else:
-    print("name doesn't exist")
+
+compfind()
+
+
+
+
+
